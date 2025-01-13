8bbe970b4d1db298fb9016aca74c30fdf044f9c1
Tien Pham <tienphamvanfuv@gmail.com>
2025-01-14 00:03:05 +0100

    Create Profile for Recruiters and Job Seekers

--- src/main/java/com/luv2code/jobportal/controller/UsersController.java
+++ src/main/java/com/luv2code/jobportal/controller/UsersController.java
@@ -2,30 +2,58 @@ package com.luv2code.jobportal.controller;
 
 import com.luv2code.jobportal.entitiy.Users;
 import com.luv2code.jobportal.entitiy.UsersType;
+import com.luv2code.jobportal.services.UsersService;
 import com.luv2code.jobportal.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UsersController {
 
     private final UsersTypeService usersTypeService;
+    private final UsersService usersService;
 
+    // constructor injection
     @Autowired
-    public UsersController(UsersTypeService theUsersTypeService) {
+    public UsersController(UsersTypeService theUsersTypeService, UsersService theUsersService) {
         this.usersTypeService = theUsersTypeService;
+        this.usersService = theUsersService;
     }
 
     @GetMapping("/register")
-    public String register(Model model) {
+    public String register(Model theModel) {
         List<UsersType> usersTypes = usersTypeService.getAll();
-        model.addAttribute("getAllTypes", usersTypes);
-        model.addAttribute("user", new Users());
+        theModel.addAttribute("getAllTypes", usersTypes);
+        theModel.addAttribute("user", new Users());
 
         return "register";
+    }
+
+    @PostMapping("register/new")
+    public String userRegistration(@Valid Users theUsers, Model theModel) {
+        System.out.println("User: " + theUsers);
+
+        Optional<Users> optionalUsers = usersService.getUserByEmail(theUsers.getEmail());
+
+        if (optionalUsers.isPresent()) {
+            theModel.addAttribute("error", "Email already registered");
+
+            List<UsersType> usersTypes = usersTypeService.getAll();
+            theModel.addAttribute("getAllTypes", usersTypes);
+            theModel.addAttribute("user", new Users());
+
+            return "register";
+
+        }
+        usersService.addNew(theUsers);
+        return "dashboard";
+
     }
 }
---
+++ src/main/java/com/luv2code/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.luv2code.jobportal.repository;
+
+import com.luv2code.jobportal.entitiy.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+}
