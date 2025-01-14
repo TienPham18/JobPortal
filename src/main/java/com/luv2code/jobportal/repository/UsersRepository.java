b557cc340313cf67dff9f96840fb867c39209dbe
Tien Pham <tienphamvanfuv@gmail.com>
2025-01-14 23:33:49 +0100

    configure Spring Security and custom authentication authorization

--- src/main/java/com/luv2code/jobportal/repository/UsersRepository.java
+++ src/main/java/com/luv2code/jobportal/repository/UsersRepository.java
@@ -7,5 +7,13 @@ import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
-    Optional<Users> findByEmail(String email);
+    Optional<Users> findByEmail(String email); // SELECT * FROM users WHERE email = ?;
+
+    /*
+    Spring Data JPA:
+    1. Parses the method name (findByEmail).
+    2. Understands that it corresponds to a query on the email field of the Users entity.
+    3. Automatically generates a query like: sql
+    SELECT * FROM users WHERE email = ?;
+     */
 }
