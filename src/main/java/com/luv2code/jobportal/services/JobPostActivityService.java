483bab08586d9e84b9b2c1ffed3d600c5c84fc42
Tien Pham <tienphamvanfuv@gmail.com>
2025-01-16 11:50:41 +0100

    Display and retrieve list of jobsfor logged-in recruiter

---
+++ src/main/java/com/luv2code/jobportal/services/JobPostActivityService.java
@@ -0,0 +1,42 @@
+package com.luv2code.jobportal.services;
+
+import com.luv2code.jobportal.entitiy.*;
+import com.luv2code.jobportal.repository.JobPostActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobPostActivityService {
+
+    private final JobPostActivityRepository jobPostActivityRepository;
+
+    @Autowired
+    public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
+        this.jobPostActivityRepository = jobPostActivityRepository;
+    }
+
+    public JobPostActivity addNew(JobPostActivity jobPostActivity) {
+        return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter) {
+
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+
+        // convert information from database to DTOs
+        for (IRecruiterJobs rec: recruiterJobsDtos) {
+
+            // Construct DTO based on Info from database
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(),
+                    rec.getJob_title(), loc, comp));
+        }
+        return recruiterJobsDtoList;
+
+    }
+}
